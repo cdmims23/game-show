8732c423e1ce2cf45709d57167c0e9c4d8cc6a4c
Cori Mims <cdmims21@gmail.com>
2020-08-23 21:18:22 -0400

    Updated phrase object and added testing code

--- js/Phrase.js
+++ js/Phrase.js
@@ -3,8 +3,25 @@ class Phrase {
         this.phrase = phrase;
     }
 
+    /**
+      * Display phrase on game board
+    */
     addPhraseToDisplay() {
+        const phraseLength = this.phrase.length;
+        const parentElement = document.querySelector('#phrase ul');
 
+        for (let i = 0; i < phraseLength; i++) {
+            let listItem = document.createElement('li');
+            if(this.phrase[i] === ' ') {
+                listItem.classList.add('space');
+            } else {
+                listItem.classList.add('hide');
+                listItem.classList.add('letter');
+                listItem.classList.add(this.phrase[i]);
+                listItem.textContent = `${this.phrase[i]};`
+            }
+            parentElement.appendChild(listItem);
+        }
     }
 
     checkLetter() {
@@ -12,6 +29,6 @@ class Phrase {
     }
 
     showMatchedLetter() {
-        
+
     }
 }
--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 const startBtn = document.querySelector('#btn_reset');
+
+
+startBtn.addEventListener('click', (e) => {
+    const game = new Game();
+    game.startGame();
+    game.getRandomPhrase().addPhraseToDisplay();
+});
