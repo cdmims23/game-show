ed7cc6bcd0e727af0910670473d82c71a9c2160f
Cori Mims <cdmims21@gmail.com>
2020-08-23 21:01:11 -0400

    Updated and tested two methods
    Updated and tested the getRandomPhrase method as well as the createPhrases method.

--- js/Game.js
+++ js/Game.js
@@ -1,7 +1,7 @@
 class Game {
     constructor() {
         this.missed = 0;
-        this.phrases = [];
+        this.phrases = this.createPhrases();
         this.activePhrase = null;
     }
 
@@ -9,8 +9,28 @@ class Game {
 
     }
 
-    getRandomPhrase() {
+    /**
+      * Creates phrases for use in game
+      * @return {array} An array of phrases that could be used in the game
+    */
+    createPhrases() {
+        const phrases = [ 
+            new Phrase('better late than never'),
+            new Phrase('break a leg'),
+            new Phrase('every cloud has a silver lining'),
+            new Phrase('the best thing since sliced bread'),
+            new Phrase('the early bird gets the worm')
+        ]
+        return phrases;
+    }
 
+    /**
+      * Selects random phrase from phrases property
+      * @return {Object} Phrase object chosen to be used
+    */
+    getRandomPhrase() {
+        const random = Math.floor(Math.random() * this.phrases.length);
+        return this.phrases[random];
     }
 
     handleInteraction() {
@@ -26,7 +46,11 @@ class Game {
     }
 
     gameOver() {
-        
+
     }
 
 }
+
+const myGame= new Game();
+
+console.log(myGame.getRandomPhrase());
